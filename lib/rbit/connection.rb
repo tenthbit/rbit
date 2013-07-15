d3bd88ee4d48a0b9f43ba7546dbb1a34399c733d
Curtis McEnroe <programble@gmail.com>
2013-07-15 04:11:43 -0400

    Strip connection down to bare protocol with handlers

--- lib/rbit/connection.rb
+++ lib/rbit/connection.rb
@@ -8,66 +8,43 @@ module Rbit
 
     PROTOCOL = '10bit'
 
-    def initialize(host, port = 10817, &block)
+    def initialize(host, port = 10817)
       @tcp_socket = TCPSocket.new(host, port)
 
       ssl_context = OpenSSL::SSL::SSLContext.new
-      if ssl_context.respond_to?(:npn_select_cb=) # Only available in Ruby 2.0
+      if ssl_context.respond_to? :npn_select_cb # Only available in Ruby 2.0
         ssl_context.npn_select_cb = proc do |protos|
-          raise ProtocolError, "Server does not support protocol #{PROTOCOL}" unless protos.include? PROTOCOL
+          raise ProtocolError, "Server does not support #{PROTOCOL}" unless protos.include? PROTOCOL
           PROTOCOL
         end
       end
       @ssl_socket = OpenSSL::SSL::SSLSocket.new(@tcp_socket, ssl_context)
 
-      @auth = {}
-
-      instance_eval(&block)
+      @handlers = {}
     end
 
-    def auth(method, data)
-      @auth[method] = data
-    end
-
-    def auth_password(username, password)
-      auth('password', {username: username, password: password})
+    def add_handler(op, handler)
+      @handlers[op] ||= []
+      @handlers[op] << handler
     end
 
     def run
       @ssl_socket.connect
-      run_loop
-    end
 
-    def run!
-      Thread.new { run }
-    end
-
-    private
-
-    def run_loop
       loop do
         raw_packet = @ssl_socket.gets
         packet = JSON.parse(raw_packet, symbolize_names: true)
 
-        p packet
+        p packet # FIXME: Log properly
 
-        case packet[:op]
-        when 'welcome'
-          authenticate(packet[:ex][:auth])
+        @handlers.fetch(packet[:op], []).each do |handler|
+          Thread.new { handler[self, packet] }
         end
       end
     end
 
-    def send(packet)
-      raw_packet = JSON.dump(packet)
-      @ssl_socket.puts(raw_packet)
-    end
-
-    def authenticate(methods)
-      @auth.each do |method, data|
-        next unless methods.include? method
-        return send({op: 'auth', ex: {method: method}.merge(data)})
-      end
+    def run!
+      Thread.new { run }
     end
   end
 end
