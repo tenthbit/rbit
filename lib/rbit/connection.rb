0f0046bc690ef6b74ebb20abab38614229146db0
Curtis McEnroe <programble@gmail.com>
2013-07-15 02:58:43 -0400

    Authenticate

--- lib/rbit/connection.rb
+++ lib/rbit/connection.rb
@@ -1,5 +1,6 @@
 require 'socket'
 require 'openssl'
+require 'json'
 
 module Rbit
   class Connection
@@ -18,14 +19,55 @@ module Rbit
         end
       end
       @ssl_socket = OpenSSL::SSL::SSLSocket.new(@tcp_socket, ssl_context)
+
+      @auth = {}
+
+      instance_eval(&block)
+    end
+
+    def auth(method, data)
+      @auth[method] = data
+    end
+
+    def auth_password(username, password)
+      auth('password', {username: username, password: password})
     end
 
     def run
       @ssl_socket.connect
+      run_loop
     end
 
     def run!
       Thread.new { run }
+    end
+
+    private
+
+    def run_loop
+      loop do
+        raw_packet = @ssl_socket.gets
+        packet = JSON.parse(raw_packet, symbolize_names: true)
+
+        p packet
+
+        case packet[:op]
+        when 'welcome'
+          authenticate(packet[:ex][:auth])
+        end
+      end
+    end
+
+    def send(packet)
+      raw_packet = JSON.dump(packet)
+      @ssl_socket.puts(raw_packet)
+    end
+
+    def authenticate(methods)
+      @auth.each do |method, data|
+        next unless methods.include? method
+        return send({op: 'auth', ex: {method: method}.merge(data)})
+      end
     end
   end
 end
